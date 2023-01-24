25befb4d233fb8c6cbce28fd8a1078f2d354b548
Winston Feng <w.fengs@gmail.com>
2023-01-24 14:15:26 -0800

    Code cleanup

--- src/components/AllComments.js
+++ src/components/AllComments.js
@@ -2,49 +2,6 @@ import { useState } from "react"
 import { CommentForm } from "./CommentForm"
 
 
-function myDeleteFunction(comment, comments, setComments) {
-    console.log("in my function", comments)
-    console.log("id", comment.id)
-    let temp = []
-    for (let i = 0; i < comments.length; i++) {
-        if (comments[i].id !== comment.id) {
-            console.log(comment.id)
-            temp.push(comments[i])
-        }
-    }
-// function checkRecursiveChildren() {
-//  //check if comment
-// //write a function where base case is no children
-// //recursive is if child id == id...
-// }
-    console.log("temp", temp)
-    setComments(temp)
-    // return (
-    //     <div>
-    //     Hello World
-    //   </div>
-    // );
-    console.log("after")
-}
-
-function updateLocalComment(id, message, setComments) {
-    setComments(prevComments => {
-      return prevComments.map(comment => {
-        if (comment.id === id) {
-          return { ...comment, message }
-        } else {
-          return comment
-        }
-      })
-    })
-}
-
-const deleteComment = (index, comment, comments, setComments) => {
-    const copy = [...comments];
-    console.log('winston')
-    copy.splice(index, 1);
-    setComments(copy);
-}
 
 const deleteComment2 = (index, comment, comments, setComments) => {
     const copy = [...comments];
@@ -66,22 +23,6 @@ function removeFromList(array, id) {
         });
 }
 
-// function handleSubmitComment(comment, comments, setComments) {
-//     // toggle item's complete flag
-//     const newComment = comment;
-//     setComments
-//   }
-
-
-function recursiveRemove ( list, id, setComments ) {
-    let temp = []
-    return list.map ( item => { return {...item} }).filter ( item => {
-        if ( 'children' in item ) {
-            item.children = recursiveRemove ( item.children, id );
-        }
-        return item.id !== id;
-    });
-}
 
 const AllComments = ({ index, comment , comments, setComments}) => {
     
@@ -103,32 +44,24 @@ const AllComments = ({ index, comment , comments, setComments}) => {
             </div>
         {isReplying ? 
           <CommentForm
-            // autoFocus
             initialValue={comment.content}
-            //onSubmit={() => update(index, 'wisnton', comments, setComments)}
             onClick={() => setIsReplying(prev => !prev)}
             comments = {comments}
             index = {index}
             setComments = {setComments}
             id = {comment.id}
             onSubmit = {'handleReply'}
-            // loading={updateCommentFn.loading}
-            // error={updateCommentFn.error}
           />
         : <div></div>}
         {isEditing ? 
           <CommentForm
-            // autoFocus
             initialValue={comment.content}
-            //onSubmit={() => update(index, 'wisnton', comments, setComments)}
             onClick={() => setIsEditing(prev => !prev)}
             comments = {comments}
             index = {index}
             setComments = {setComments}
             id = {comment.id}
             onSubmit = {'handleEdit'}
-            // loading={updateCommentFn.loading}
-            // error={updateCommentFn.error}
           />
         : <div></div>}
         <div>{comment.children?.map(comment =>
@@ -138,6 +71,5 @@ const AllComments = ({ index, comment , comments, setComments}) => {
         </div>
     )
 }
-//need to create a way to conditionally show edit/delete if the comment is not a root comment.
-//onclick  
+
 export default AllComments
